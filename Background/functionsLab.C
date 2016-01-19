9affc1d2eb42b612dfbf99dfd6dd339db42ff3e1
J-C-Wright <jack.charles.wright@cern.ch>
2016-01-19 17:37:12 +0100

    Adds output log and ATLAS functions, Dijet functions, amongst others to functionsLab.C

--- Background/functionsLab.C
+++ Background/functionsLab.C
@@ -1,188 +1,295 @@
 #include <vector>
-void functionsLab() {
+#include<iostream>
 
-    TFile *output = new TFile("FitPlots/BackgroundFits.root","RECREATE");
+    void functionsLab() {
 
-    TFile *_file0 = TFile::Open("/afs/cern.ch/user/m/musella/public/workspace/exo/full_analysis_spring15_7415v2_sync_v5_data_ecorr_cic2_final_ws.root");
-    t_EBEE=_file0->Get("tree_data_cic2_EBEE");
-    t_EBEB=_file0->Get("tree_data_cic2_EBEB");
-    //TCanvas *t = new TCanvas ("functionLab","functionLab",800,400);
-    //t->Divide(2,1);
-    gStyle->SetOptStat(11111);
-    gStyle->SetOptFit(11111);
+//        TFile *output = new TFile("FitPlots/BackgroundFits.root","RECREATE");
 
-    //t->cd(1);
-    t_EBEE->Draw("mgg>>h_EBEE(100,320,1600)");
-    TH1F *h_EBEE = (TH1F*)gPad->GetPrimitive("h_EBEE");
-    h_EBEE->SetMarkerColor(kBlack);
-    h_EBEE->SetMarkerSize(4);
-    h_EBEE->Draw("EP");
+        TFile *_file0 = TFile::Open("/afs/cern.ch/user/m/musella/public/workspace/exo/full_analysis_spring15_7415v2_sync_v5_data_ecorr_cic2_final_ws.root");
+        t_EBEE=_file0->Get("tree_data_cic2_EBEE");
+        t_EBEB=_file0->Get("tree_data_cic2_EBEB");
+        //TCanvas *t = new TCanvas ("functionLab","functionLab",800,400);
+        //t->Divide(2,1);
+        gStyle->SetOptStat(11111);
+        gStyle->SetOptFit(11111);
 
-    //t->cd(2);
-    t_EBEB->Draw("mgg>>h_EBEB(100,230,1600)");
-    TH1F *h_EBEB = (TH1F*)gPad->GetPrimitive("h_EBEB");
-    h_EBEB->SetMarkerColor(kBlack);
-    h_EBEB->Draw("EP");
+        //t->cd(1);
+        t_EBEE->Draw("mgg>>h_EBEE(34,320,1000)");
+        TH1F *h_EBEE = (TH1F*)gPad->GetPrimitive("h_EBEE");
+        h_EBEE->SetMarkerColor(kBlack);
+        h_EBEE->SetMarkerSize(4);
+        h_EBEE->Draw("EP");
 
-    output->cd();
-    h_EBEE->Write();
-    h_EBEB->Write();
-    output->Close();
+        //t->cd(2);
+        t_EBEB->Draw("mgg>>h_EBEB(34,230,910)");
+        TH1F *h_EBEB = (TH1F*)gPad->GetPrimitive("h_EBEB");
+        h_EBEB->SetMarkerColor(kBlack);
+        h_EBEB->Draw("EP");
 
 
+    //-------------------
+    //Functions and fits
+    //-------------------
+        TCanvas c1("c1");
+        c1.SetLogy();
 
-//-------------------
-//Functions and fits
-//-------------------
-    TCanvas c1("c1");
-    h_EBEB->SetStats(kFALSE);
+        h_EBEB->SetStats(kFALSE);
 
-//EBEB Region
+    //EBEB Region
+        std::ofstream EBEB_FitsLog;
+        EBEB_FitsLog.open("FitPlots/EBEB_Fits.txt");
+        EBEB_FitsLog << setw(24) << "Name" << setw(12) << "ChiSq" << setw(12) << "NDOF" << setw(12) << "ChiSq/NDOF" << std::endl;
 
-//Polynomial fits
+    //Polynomial fits
+        //Power basis
+        EBEB_FitsLog << "Polynomials - Power basis" << std::endl;
+        std::vector<TF1*> powerBasis(2);
+        for (unsigned i(0);i<3;i++) {
+            powerBasis[i] = new TF1(Form("polyPB%d",i+1),Form("pol%d",i+1),230,910);
+            powerBasis[i]->SetLineColor(i+1);
 
-    
+            h_EBEB->Fit(Form("polyPB%d",i+1),"QRM");
+            EBEB_FitsLog << setw(24) << powerBasis[i]->GetName() << setw(12) << powerBasis[i]->GetChisquare() << setw(12) << powerBasis[i]->GetNDF();
+            EBEB_FitsLog << setw(12) << powerBasis[i]->GetChisquare()/powerBasis[i]->GetNDF() << std::endl;
+        }
 
-    //Power basis
-    std::vector<TF1*> powerBasis(2);
-    for (unsigned i(0);i<3;i++) {
-        powerBasis[i] = new TF1(Form("polyPB%d",i+1),Form("pol%d",i+1),230,910);
-        powerBasis[i]->SetLineColor(i+1);
+        h_EBEB->Draw("EP");
+        for (unsigned i(0);i<3;i++) {
+            powerBasis[i]->Draw("SAME");
+        }
+        c1.Print("FitPlots/Polys_PowerBasis.pdf");
 
-        h_EBEB->Fit(Form("polyPB%d",i+1),"RM");
+        //Bernstein basis
+        EBEB_FitsLog << "Polynomials - Bernstein sub-basis (i<n/2)" << std::endl;
+        std::vector<TF1*> bernsteinBasis(4);
+        h_EBEB->Draw("EP");
+        for (unsigned i(0);i<5;i++) {
+
+            TString functionString = SubbasisBernsteinString(i+1,230,910);
+            bernsteinBasis[i] = new TF1(Form("Bern_D%d",i+1),functionString,230,910);
+            bernsteinBasis[i]->SetLineColor(i+1);
+
+            h_EBEB->Fit(Form("Bern_D%d",i+1),"QRM");
+            EBEB_FitsLog << setw(24) << bernsteinBasis[i]->GetName() << setw(12) << bernsteinBasis[i]->GetChisquare() << setw(12) << bernsteinBasis[i]->GetNDF();
+            EBEB_FitsLog << setw(12) << bernsteinBasis[i]->GetChisquare()/bernsteinBasis[i]->GetNDF() << std::endl;
+        }
+
+        h_EBEB->Draw("EP");
+        for (unsigned i(0);i<5;i++) {
+            bernsteinBasis[i]->Draw("SAME");
+        }
+        c1.Print("FitPlots/Polys_BernsteinBasis.pdf");
+
+        //Convex basis pairs
+        EBEB_FitsLog << "Polynomials - Convex sub-basis" << std::endl;
+        std::vector<TF1*> convexPairs(45); 
+        unsigned count(0);
+        for (unsigned i(0);i<10;i++) {
+            for (unsigned j(0);j<10;j++) {
+                if ( j >= i ) continue;
+                convexPairs[count] = new TF1(Form("Convex_%d_%d",i,j),ConvexPolyPairString(i,j,230,910),230,910);
+                count++;
+            }
+        }
+        for (unsigned i(0);i<convexPairs.size();i++) {
+            h_EBEB->Fit( convexPairs[i]->GetName(),"QRM");
+            EBEB_FitsLog << setw(24) << convexPairs[i]->GetName() << setw(12) << convexPairs[i]->GetChisquare() << setw(12) << convexPairs[i]->GetNDF();
+            EBEB_FitsLog << setw(12) << convexPairs[i]->GetChisquare()/convexPairs[i]->GetNDF() << std::endl;
+        }
+        h_EBEB->Draw("EP");
+        for (unsigned i(0);i<convexPairs.size();i++) {
+            convexPairs[i]->Draw("SAME");
+        }
+        h_EBEB->Draw("EPSAME");
+        c1.Print("FitPlots/Polys_ConvexPairs.pdf");
+
+    //Power Laws
+        //Single power law
+        EBEB_FitsLog << "Power Laws" << std::endl;
+        TF1 *powerlaw = new TF1("power","[0]*pow(x,[1])",230,910);
+        h_EBEB->Fit("power","QRM");
+        EBEB_FitsLog << setw(24) << powerlaw->GetName() << setw(12) << powerlaw->GetChisquare() << setw(12) << powerlaw->GetNDF();
+        EBEB_FitsLog << setw(12) << powerlaw->GetChisquare()/powerlaw->GetNDF() << std::endl;
+        h_EBEB->Draw("EP");
+        powerlaw->Draw("SAME");
+        c1.Print("FitPlots/PowerLaws.pdf");
+
+    //Exponentials
+        //Single exponential
+        EBEB_FitsLog << "Exponentials" << std::endl;
+        TF1 *exponent = new TF1("exponent","exp([0] + x*[1])",230,910);
+        h_EBEB->Fit("exponent","QRM");
+        EBEB_FitsLog << setw(24) << exponent->GetName() << setw(12) << exponent->GetChisquare() << setw(12) << exponent->GetNDF();
+        EBEB_FitsLog << setw(12) << exponent->GetChisquare()/exponent->GetNDF() << std::endl;
+        h_EBEB->Draw("EP");
+        exponent->Draw("SAME");
+        c1.Print("FitPlots/Exponentials.pdf");
+
+    //Laurent
+
+
+
+
+    //ATLAS Function
+        EBEB_FitsLog << "ATLAS functions" << std::endl;
+        std::vector<TF1*> atlasFunctions(3);
+        for (unsigned k(0);k<atlasFunctions.size();k++){
+            std::cout << ATLASString(k) << std::endl;
+            atlasFunctions[k] = new TF1(Form("Atlas%d",k),ATLASString(k),230,910);
+            h_EBEB->Fit(Form("Atlas%d",k),"RM");
+            EBEB_FitsLog << setw(24) << atlasFunctions[k]->GetName() << setw(12) << atlasFunctions[k]->GetChisquare() << setw(12) << atlasFunctions[k]->GetNDF();
+            EBEB_FitsLog << setw(12) << atlasFunctions[k]->GetChisquare()/atlasFunctions[k]->GetNDF() << std::endl;
+        }
+        h_EBEB->Draw("EP");
+        for (unsigned k(0);k<atlasFunctions.size();k++) {
+            atlasFunctions[k]->SetLineColor(k+1);
+            atlasFunctions[k]->Draw("SAME");
+        }
+        c1.Print("FitPlots/ATLASFunctions.pdf");
+
+    //Dijet Function
+        EBEB_FitsLog << "Dijet functions" << std::endl;
+        std::vector<TF1*> dijetFunctions(4);
+        for (unsigned k(0);k<4;k++) {
+            std::cout << DijetString(k+1) << std::endl;
+            dijetFunctions[k] = new TF1(Form("Dijet%d",k+1),DijetString(k+1),230,910);
+            h_EBEB->Fit(Form("Dijet%d",k+1),"RM");
+            EBEB_FitsLog << setw(24) << dijetFunctions[k]->GetName() << setw(12) << dijetFunctions[k]->GetChisquare() << setw(12) << dijetFunctions[k]->GetNDF();
+            EBEB_FitsLog << setw(12) << dijetFunctions[k]->GetChisquare()/dijetFunctions[k]->GetNDF() << std::endl;
+        }
+        h_EBEB->Draw("EP");
+        for (unsigned k(0);k<dijetFunctions.size();k++) {
+            dijetFunctions[k]->SetLineColor(k+1);
+            dijetFunctions[k]->Draw("SAME");
+        }
+        c1.Print("FitPlots/DijetFunctions.pdf");
+       
     }
 
-    h_EBEB->Draw("EP");
-    for (unsigned i(0);i<3;i++) {
-        powerBasis[i]->Draw("SAME");
-    }
-    h_EBEB->Draw("SAME");
-    c1.Print("FitPlots/Polys_PowerBasis.pdf");
-
-    //Bernstein basis
-    std::vector<TF1*> bernsteinBasis(4);
-    h_EBEB->Draw("EP");
-    for (unsigned i(0);i<5;i++) {
-
-        std::cout << "\nMaking and fitting Bernstein basis polynomial of degree " << i+1 << " function string is:" << std::endl;
-        TString functionString = SubbasisBernsteinString(i+1,230,910);
-        std::cout << functionString << std::endl;
-
-        bernsteinBasis[i] = new TF1(Form("Bern_D%d",i+1),functionString,230,910);
-        bernsteinBasis[i]->SetLineColor(i+1);
-
-        h_EBEB->Fit(Form("Bern_D%d",i+1),"RM");
-    }
-
-    h_EBEB->Draw("EP");
-    for (unsigned i(0);i<5;i++) {
-        bernsteinBasis[i]->Draw("SAME");
-    }
-    h_EBEB->Draw("SAME");
-    c1.Print("FitPlots/Polys_BernsteinBasis.pdf");
-
-    //(1-t)^n functions
-    std::vector<TF1*> mdBasis(3);
-    h_EBEB->Draw("EP");
-    for (unsigned i(0);i<3;i++) {
-        std::cout << MonoDecreaseString(i,230,910) << std::endl << std::endl;
-        mdBasis[i] = new TF1(Form("mdBasis%d",i),MonoDecreaseString(i,230,910),230,910);
-        mdBasis[i]->SetLineColor(i+1);
-        h_EBEB->Fit(Form("mdBasis%d",i),"RM");
-        mdBasis[i]->Draw("SAME");
-    }
-    h_EBEB->Draw("SAME");
-    c1.Print("FitPlots/Polys_Convex.pdf");
-
-//Power Laws
-
-    TF1 *powerlaw = new TF1("power","[0]*pow(x,[1])",230,910);
-    h_EBEB->Fit("power","RM");
-    h_EBEB->Draw("EP");
-    powerlaw->Draw("SAME");
-    h_EBEB->Draw("SAME");
-    c1.Print("FitPlots/PowerLaws.pdf");
-
-//Exponentials
-
-    TF1 *exponent = new TF1("exponent","exp([0] + x*[1])",230,910);
-    h_EBEB->Fit("exponent","RM");
-    h_EBEB->Draw("EP");
-    exponent->Draw("SAME");
-    h_EBEB->Draw("SAME");
-    c1.Print("FitPlots/Exponentials.pdf");
-
-//Laurent
 
 
 
-//From outside
-    TF1 *other = new TF1("other","x*( [0] + [1]*TMath::Log(x) )",230,910);
-    h_EBEB->Fit("other","RM");
-    h_EBEB->Draw("EP");
-    other->Draw("SAME");
-    c1.Print("FitPlots/Other.pdf");
-    
-}
 
-TString BernsteinString(unsigned n, float xMin, float xMax) {
 
-    if (n == 0) {return TString("[0]");}
 
-    std::ostringstream poly;
 
-    for (unsigned i(0);i<n+1;i++) {
 
-        unsigned factorialPart = (unsigned)TMath::Factorial(n)/( TMath::Factorial(i)*TMath::Factorial(n-i) );
 
-        poly << "[" << i << "]*";
-        if (factorialPart > 1) {poly << factorialPart << ".0*";}
-        if (i > 1) {poly << "pow((x - " << xMin << ")/" << xMax << "," << i << ")";} else if (i == 1) {poly << "((x-" << xMin << ")/" << xMax << ")";}
-        if (n != i && i != 0) {poly << "*";}
-        if (n-i > 1) {poly << "pow(1-(x-" << xMin << ")/" << xMax << "," << n-i << ")";} else if (n-i == 1) {poly << "(1-(x-" << xMin << ")/" << xMax  << ")";}
-        if (i < n) {poly << " + ";}
-    }
-    TString output = poly.str();
 
-    return output;
-};
 
-TString SubbasisBernsteinString(unsigned n, float xMin, float xMax) {
+    TString BernsteinString(unsigned n, float xMin, float xMax) {
 
-    if (n == 0) {return TString("[0]");}
-    
-    std::ostringstream poly;
+        if (n == 0) {return TString("[0]");}
 
-    unsigned iLimit = n/2;
+        std::ostringstream poly;
 
-    for (unsigned i(0);i<iLimit+1;i++) {
+        for (unsigned i(0);i<n+1;i++) {
 
-        unsigned factorialPart = (unsigned)TMath::Factorial(n)/( TMath::Factorial(i)*TMath::Factorial(n-i) );
+            unsigned factorialPart = (unsigned)TMath::Factorial(n)/( TMath::Factorial(i)*TMath::Factorial(n-i) );
 
-        poly << "[" << i << "]*";
-        if (factorialPart > 1) {poly << factorialPart << ".0*";}
-        if (i > 1) {poly << "pow((x - " << xMin << ")/" << xMax << "," << i << ")";} else if (i == 1) {poly << "((x-" << xMin << ")/" << xMax << ")";}
-        if (n != i && i != 0) {poly << "*";}
-        if (n-i > 1) {poly << "pow(1-(x-" << xMin << ")/" << xMax << "," << n-i << ")";} else if (n-i == 1) {poly << "(1-(x-" << xMin << ")/" << xMax  << ")";}
-        if (i < iLimit) {poly << " + ";}
-    }
-    TString output = poly.str();
+            poly << "[" << i << "]*";
+            if (factorialPart > 1) {poly << factorialPart << ".0*";}
+            if (i > 1) {poly << "pow((x - " << xMin << ")/" << xMax << "," << i << ")";} else if (i == 1) {poly << "((x-" << xMin << ")/" << xMax << ")";}
+            if (n != i && i != 0) {poly << "*";}
+            if (n-i > 1) {poly << "pow(1-(x-" << xMin << ")/" << xMax << "," << n-i << ")";} else if (n-i == 1) {poly << "(1-(x-" << xMin << ")/" << xMax  << ")";}
+            if (i < n) {poly << " + ";}
+        }
+        TString output = poly.str();
 
-    return output;
-};
+        return output;
+    };
 
-TString MonoDecreaseString(unsigned n, float xMin, float xMax) {
+    TString SubbasisBernsteinString(unsigned n, float xMin, float xMax) {
 
-    std::ostringstream poly;
-    for (unsigned i(0);i<n+1;i++) {
-        poly << "[" << i << "]*pow(1-(x-" << xMin << ")/" << xMax << "," << i << ")";
-        if (i != n) poly << " + ";
-    }
-    TString output = poly.str();
+        if (n == 0) {return TString("[0]");}
+        
+        std::ostringstream poly;
 
-    return output;
-};
-    
+        unsigned iLimit = n/2;
+
+        for (unsigned i(0);i<iLimit+1;i++) {
+
+            unsigned factorialPart = (unsigned)TMath::Factorial(n)/( TMath::Factorial(i)*TMath::Factorial(n-i) );
+
+            poly << "[" << i << "]*";
+            if (factorialPart > 1) {poly << factorialPart << ".0*";}
+            if (i > 1) {poly << "pow((x - " << xMin << ")/" << xMax << "," << i << ")";} else if (i == 1) {poly << "((x-" << xMin << ")/" << xMax << ")";}
+            if (n != i && i != 0) {poly << "*";}
+            if (n-i > 1) {poly << "pow(1-(x-" << xMin << ")/" << xMax << "," << n-i << ")";} else if (n-i == 1) {poly << "(1-(x-" << xMin << ")/" << xMax  << ")";}
+            if (i < iLimit) {poly << " + ";}
+        }
+        TString output = poly.str();
+
+        return output;
+    };
+
+    TString ConvexPolyString(unsigned start, unsigned end, float xMin, float xMax) {
+
+        std::ostringstream poly;
+        for (unsigned i(start);i<end+1;i++) {
+            poly << "[" << i-start << "]*pow(1-(x-" << xMin << ")/" << xMax << "," << i << ")";
+            if (i != end) poly << " + ";
+        }
+        TString output = poly.str();
+
+        return output;
+    };
+        
+    TString ConvexPolyPairString(unsigned n, unsigned m, float xMin, float xMax) {
+
+        std::ostringstream poly;
+        
+        poly << "[0]*pow(1-(x-" << xMin << ")/" << xMax << ", " << n << ")";
+        poly << " + [1]*pow(1-(x-" << xMin << ")/" << xMax << ", " << m << ")";
+
+        TString output = poly.str();
+
+        return output;
+    };
+
+    TString ATLASString(unsigned k) {
+
+        std::ostringstream atlas;
+
+        atlas << "pow( (1-pow(x/13000.0,1.0/3.0)), [0])";
+        if (k==0){
+            atlas << "*pow(x/13000.0,[1])";
+        }else{
+            atlas << "*pow(x/13000.0,[1]";
+            for (unsigned i(1);i<k+1;i++){
+                atlas << " + [" << i+1 << "]*pow(TMath::Log(x/13000.0)," << i << ")";
+            }
+            atlas << ")";
+        }
+        TString output = atlas.str();
+
+        return output;
+    };
+
+    TString DijetString(unsigned k) {
+
+        std::ostringstream dijet;
+
+        dijet << "TMath::Exp(";
+        if (k == 0) {
+            dijet << " [0]";
+        }else{
+            dijet << " [0]";
+            for (unsigned i(1);i<k+1;i++) {
+                dijet << " + [" << i << "]*pow(TMath::Log(x)," << i << ")";
+            }
+        }
+        dijet << " )";
+        TString output = dijet.str();
+
+        return output;
+    };
+
+
+
+
+
+
 
 
 
